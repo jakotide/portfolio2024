40ce2e8ee60a3252947f82c314e0a521947587d9
Jakob Tidemand <111381232+jakotide@users.noreply.github.com>
2024-06-21 14:24:31 +0200

    Circle transition

--- app/components/morework/page.jsx
+++ app/components/morework/page.jsx
@@ -1,27 +1,63 @@
 import styles from "./morework.module.scss";
-import { useScroll, useTransform, motion } from "framer-motion";
-import { useRef } from "react";
-
-import React from "react";
+import {
+  useScroll,
+  useTransform,
+  motion,
+  useAnimationControls,
+} from "framer-motion";
+import React, { useRef, useEffect, useState } from "react";
 
 export const MoreWork = () => {
   const container = useRef(null);
+  // const controls = useAnimationControls();
 
   const { scrollYProgress } = useScroll({
     target: container,
-    offset: ["start end", "200% start"],
+    offset: ["-300px end", "end start"],
   });
+  const height = useTransform(scrollYProgress, [0, 0.5], [0, 90]);
+  // const scale = useTransform(scrollYProgress, [0, 0.3], [0.5, 1]);
 
-  const width = useTransform(scrollYProgress, [0, 0.27], ["50%", "100%"]);
+  // useEffect(() => {
+  //   scrollYProgress.onChange((progress) => {
+  //     if (progress > 0.1) {
+  //       controls.start("visible");
+  //       console.log("hello");
+  //     } else if (progress < 0.3) {
+  //       controls.start("hidden");
+  //     }
+  //   });
+  // }, [scrollYProgress, controls]);
+
+  // const fadeIn = {
+  //   hidden: { opacity: 0, filter: "blur(20px)" },
+  //   visible: { opacity: 1, filter: "blur(0)" },
+  // };
+
   return (
-    <motion.section
-      className={styles.more__section}
-      style={{ width }}
-      ref={container}
-    >
-      <div className={styles.more__content}>
-        <h1 className={styles.more__h1}>More Projects</h1>
-      </div>
-    </motion.section>
+    <div className={styles.test} ref={container}>
+      <motion.div className={styles.circle__cont} style={{ height }}>
+        <div className={styles.circle}></div>
+      </motion.div>
+      <motion.section
+        className={styles.more__section}
+        // style={{ scale }}
+        // ref={container}
+      >
+        {/* <div className={styles.circle__cont}>
+          <div className={styles.circle}></div>
+        </div> */}
+        {/* <div className={styles.radial__gradient}></div> */}
+        <motion.div
+          className={styles.more__content}
+          // variants={fadeIn}
+          // animate={controls}
+          // initial="hidden"
+          // transition={{ duration: 0.5 }}
+        >
+          <h1 className={styles.more__h1}>More Projects</h1>
+        </motion.div>
+      </motion.section>
+    </div>
   );
 };
