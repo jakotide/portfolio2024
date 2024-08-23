324df62615f8f0b2681017c519ce58dc2ec7efec
Jakob Tidemand <111381232+jakotide@users.noreply.github.com>
2024-08-23 19:10:48 +0200

    Build test

--- app/components/effects/index.jsx
+++ app/components/effects/index.jsx
@@ -1,8 +1,8 @@
 export { Cursor } from "./cursorfollow/cursorfollow.jsx";
 export { LenisSmoothScroll } from "./lenissmoothscroll.jsx";
 export { Preloader } from "./preloader/preloader.jsx";
-export { TransitionLink } from "./transitionlink.jsx";
+export { TransitionLink } from "./transitionlink";
 export { PageTransition } from "./pagetransition/pagetransition.jsx";
-export { ModalHover } from "./modalhover/modalhover.jsx";
+export { ModalHover } from "./modalhover/modalhover";
 export { BlurReveal } from "./blurreveal.jsx";
 // Testing 2
