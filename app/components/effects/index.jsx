6ba10e4cb43cd14bd42df7bd0384d32a2d890d1d
Jakob Tidemand <111381232+jakotide@users.noreply.github.com>
2024-08-23 16:23:39 +0200

    Removed Gravity Canvas

--- app/components/effects/index.jsx
+++ app/components/effects/index.jsx
@@ -5,5 +5,4 @@ export { Preloader } from "./preloader/page";
 export { TransitionLink } from "./transitionlink/page";
 export { PageTransition } from "./pagetransition/page";
 export { ModalHover } from "./modalHover/page";
-export { GravityCanvas } from "./gravityCanvas/page";
 export { Overlay } from "./navoverlay/page";
